0044611de734ffce07849936362f6d7cc14713c0
UAMI-Development. <47373097+dsch-sdip@users.noreply.github.com>
2026-07-24 13:23:16 -0600

    refactor(bootstrap)
    Decouple framework bootstrap from WordPress

--- bootstrap.php
+++ bootstrap.php
@@ -8,7 +8,7 @@ declare(strict_types=1);
  *
  * File........: bootstrap.php
  * Component...: Bootstrap Loader
- * Version.....: 1.0.0-alpha
+ * Version.....: 1.0.1
  * PHP.........: >= 8.2
  *
  * Description:
@@ -25,11 +25,15 @@ declare(strict_types=1);
  * =============================================================================
  */
 
-defined('ABSPATH') || exit;
-
-const LSS_ROOT = '/opt/lss';
+if (!defined('LSS_ROOT')) {
+    define('LSS_ROOT', '/opt/lss');
+}
 
 require_once LSS_ROOT . '/version.php';
 require_once LSS_ROOT . '/autoload.php';
+
+if (!class_exists(\LSS\Core\Application::class)) {
+    throw new RuntimeException('Unable to load LSS Core Application.');
+}
 
 LSS\Core\Application::boot();
